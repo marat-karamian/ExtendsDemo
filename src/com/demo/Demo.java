6906f3946772ce33ec61b221c52d79cc2866c214
marat-karamian <marat_karamian@epam.com>
2018-06-17 19:00:47 +0300

    EventHandler

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -5,7 +5,14 @@ public class Demo {
     public static void main(String[] args) {
         Button b = new Button();
 
-        b.addClickHandler(new MyEventProcessor());
+        b.addClickHandler(new EventHandler() {
+            @Override
+            public void onClick() {
+                System.out.println("Anon click");
+            }
+        });
+
+
         b.addActionHadler(new MyEventProcessor());
         b.action();
     }
